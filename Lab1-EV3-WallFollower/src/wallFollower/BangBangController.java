e0f03ccc71597ec73de7a7ca57198483e1b38108
nlevine21 <noahlevine21@gmail.com>
2017-01-13 17:54:00 -0500

    working versions of ptype and bangbang

--- Lab1-EV3-WallFollower/src/wallFollower/BangBangController.java
+++ Lab1-EV3-WallFollower/src/wallFollower/BangBangController.java
@@ -6,9 +6,11 @@ public class BangBangController implements UltrasonicController{
 	private final int motorLow, motorHigh;
 	private int distance;
 	private EV3LargeRegulatedMotor leftMotor, rightMotor;
-	
-	private int filterControl; 
 	private final int FILTER_OUT = 20;
+	private int filterControl;
+	
+	private int count;
+	private final int COUNT_BOUND = 40;
 	
 	public BangBangController(EV3LargeRegulatedMotor leftMotor, EV3LargeRegulatedMotor rightMotor,
 							  int bandCenter, int bandwidth, int motorLow, int motorHigh) {
@@ -19,39 +21,39 @@ public class BangBangController implements UltrasonicController{
 		this.motorHigh = motorHigh;					//Upper speed for motor
 		this.leftMotor = leftMotor;					//Speed of left motor
 		this.rightMotor = rightMotor;				// Speed of right motor
-		
-		
 		filterControl = 0;
+		
+		leftMotor.setSpeed(motorHigh);				// Start robot moving forward
+		rightMotor.setSpeed(motorHigh);				
+		leftMotor.forward();
+		rightMotor.forward();
 	}
 	
 	@Override
 	public void processUSData(int distance) {
-	
-			
-		// rudimentary filter - toss out invalid samples corresponding to null
-				// signal.
-				// (n.b. this was not included in the Bang-bang controller, but easily
-				// could have).
-				//
-				if (distance >= 255 && filterControl < FILTER_OUT) {
-					// bad value, do not set the distance var, however do increment the
-					// filter value
-					filterControl++;
-				} else if (distance >= 255) {
-					// We have repeated large values, so there must actually be nothing
-					// there: leave the distance alone
-					this.distance = distance;
-				} else {
-					// distance went below 255: reset filter and leave
-					// distance alone.
-					filterControl = 0;
-					this.distance = distance;
-				}
+		
+		if (distance >= 255 && filterControl < FILTER_OUT) {
+			// bad value, do not set the distance var, however do increment the
+			// filter value
+			filterControl++;
+		} else if (distance >= 255) {
+			// We have repeated large values, so there must actually be nothing
+			// there: leave the distance alone
+			this.distance = distance;
+		} else {
+			// distance went below 255: reset filter and leave
+			// distance alone.
+			filterControl = 0;
+			this.distance = distance;
+		}
 
+		
+		
+		
 		
 		// TODO: process a movement based on the us distance passed in (BANG-BANG style)
 		
-		int distError = bandCenter - this.distance; 		//Measured error from desired distance
+		int distError = bandCenter - distance; 		//Measured error from desired distance
 		
 		if (Math.abs(distError) <= bandwidth) {
 			leftMotor.setSpeed(motorHigh);				
@@ -60,30 +62,49 @@ public class BangBangController implements UltrasonicController{
 			rightMotor.forward();
 		}
 		
-		else if (distError > 0) { 
+		else if (distError > 0) {
+			
+			if(count < COUNT_BOUND){
+				count++;
+				leftMotor.setSpeed(motorHigh);
+				rightMotor.setSpeed(motorHigh);
+				leftMotor.forward();
+				rightMotor.forward();
+				return;
+			}
+			
+			else {
 			
 			if (Math.abs (distError )< 20)
 			{
 				leftMotor.setSpeed (motorHigh);
 				rightMotor.setSpeed(motorLow);
+				
+				if (Math.abs (distError) < 10)
+				{
+					leftMotor.setSpeed(motorHigh);
+					rightMotor.setSpeed( 0 );
+				}
+				
 				leftMotor.forward();
 				rightMotor.backward();
+				
+				
 				
 			}
 			
 			else
 				
-			{ 
-			
+			{
 			leftMotor.setSpeed(motorHigh);
 			rightMotor.setSpeed(motorLow);
 			leftMotor.forward();
 			rightMotor.forward();
 			}
-		}
 			
-	
+			}
 				
+		}
 		
 		else if (distError < 0) {
 	
@@ -92,7 +113,14 @@ public class BangBangController implements UltrasonicController{
 			{
 				leftMotor.setSpeed (motorLow);
 				rightMotor.setSpeed(motorHigh);
-				leftMotor.backward();
+				
+				if (Math.abs (distError) < 10)
+				{
+					leftMotor.setSpeed(0);
+					rightMotor.setSpeed( motorHigh );
+				}
+				
+				leftMotor.forward();
 				rightMotor.forward();
 			}
 			
@@ -102,12 +130,11 @@ public class BangBangController implements UltrasonicController{
 			leftMotor.setSpeed(motorLow);
 			rightMotor.setSpeed(motorHigh);
 			leftMotor.forward();
-			rightMotor.forward();
-			}
+			rightMotor.forward();}
 		}
+		
+		
 	}
-		
-		
 
 	@Override
 	public int readUSDistance() {
--- Lab1-EV3-WallFollower/src/wallFollower/PController.java
+++ Lab1-EV3-WallFollower/src/wallFollower/PController.java
@@ -5,19 +5,22 @@ public class PController implements UltrasonicController {
 	
 	private final int bandCenter, bandwidth;
 	private final int motorStraight = 200, FILTER_OUT = 20;
+	private final int countBound = 30; //for countTimesDistanceOutOfRange, 40 equals to 2s.
+	private final int PConstant = 10;
+	private int MaxCorrection = 100;
 	private EV3LargeRegulatedMotor leftMotor, rightMotor;
 	private int distance;
 	private int filterControl;
+	int count = 0;
+	
 	
 	public PController(EV3LargeRegulatedMotor leftMotor, EV3LargeRegulatedMotor rightMotor,
 					   int bandCenter, int bandwidth) {
 		//Default Constructor
-		this.bandCenter = bandCenter - 5;
+		this.bandCenter = bandCenter;
 		this.bandwidth = bandwidth;
 		this.leftMotor = leftMotor;
 		this.rightMotor = rightMotor;
-		
-		
 		leftMotor.setSpeed(motorStraight);					// Initalize motor rolling forward
 		rightMotor.setSpeed(motorStraight);
 		leftMotor.forward();
@@ -27,82 +30,79 @@ public class PController implements UltrasonicController {
 	
 	@Override
 	public void processUSData(int distance) {
-
-		// rudimentary filter - toss out invalid samples corresponding to null
-		// signal.
-		// (n.b. this was not included in the Bang-bang controller, but easily
-		// could have).
+		/*
+		// rudimentary filter - toss out invalid samples corresponding to null signal.
+		// (n.b. this was not included in the Bang-bang controller, but easily could have).
 		//
-		if (distance >= 255 && filterControl < FILTER_OUT) {
-			// bad value, do not set the distance var, however do increment the
-			// filter value
-			filterControl++;
-		} else if (distance >= 255) {
-			// We have repeated large values, so there must actually be nothing
-			// there: leave the distance alone
-			this.distance = distance;
+		if (distance > 255 && filterControl < FILTER_OUT) {
+			// bad value, do not set the distance var, however do increment the filter value
+			filterControl ++;
+		} else if (distance > 255){
+			// true 255, therefore set distance to 255
+			this.distance = 255;
 		} else {
-			// distance went below 255: reset filter and leave
-			// distance alone.
+			// distance went below 255, therefore reset everything.
 			filterControl = 0;
 			this.distance = distance;
 		}
+		*/
 		
+		int error = (distance - bandCenter);
 		
-		int distError = bandCenter - this.distance;
-		int k = 2;
-		
-		int speedFactor = Math.abs(k*distError);
-		
-		if (Math.abs(distError) <= bandwidth) {		// Within limits, same speed
-			leftMotor.setSpeed(motorStraight);		// Start moving forward
+		if(Math.abs(error) <= bandwidth){
+			count = 0;
+			leftMotor.setSpeed(motorStraight);
 			rightMotor.setSpeed(motorStraight);
 			leftMotor.forward();
-			rightMotor.forward();				
+			rightMotor.forward();
 		}
 		
-		else if (distError > 0) {					// Too close to the wall
-			
-			if (Math.abs (distError )< 20)
-			{
-				leftMotor.setSpeed (motorStraight+speedFactor);
-				rightMotor.setSpeed(motorStraight - speedFactor);
-				leftMotor.forward();
-				rightMotor.backward();
-				
-			}
-			
-			else {
-				
-			leftMotor.setSpeed(motorStraight+speedFactor);	//sets new speed
-			rightMotor.setSpeed(motorStraight-speedFactor);				//sets new speed
-			leftMotor.forward();
-			rightMotor.forward();		
-			}
-			
+		int dif = calcP(error);
+		
+		
+		//too close to the wall
+		if (error<0){
+					count = 0;
+					leftMotor.setSpeed(motorStraight+2*dif);
+					rightMotor.setSpeed(motorStraight-dif);
+					leftMotor.forward();
+					rightMotor.backward();
 		}
 		
-		else if (distError < 0) {
-			
-			if (Math.abs(distError) < 20) {
-				leftMotor.setSpeed (motorStraight - speedFactor);
-				rightMotor.setSpeed(motorStraight + speedFactor);
+		//too far from the wall
+		if (error>0) {
+			// filter checks for gaps and avoids cutting corners
+			if(count < countBound){
+				count++;
+				leftMotor.setSpeed(motorStraight);
+				rightMotor.setSpeed(motorStraight);
 				leftMotor.forward();
-				rightMotor.backward();
+				rightMotor.forward();
+				return;
 			}
-			
 			else {
-				
-			
-			leftMotor.setSpeed(motorStraight-speedFactor); //keeps wheel moving straight
-			rightMotor.setSpeed(motorStraight+speedFactor); // sets new speed
+			leftMotor.setSpeed(motorStraight);
+			rightMotor.setSpeed(motorStraight+2*dif);
 			leftMotor.forward();
-			rightMotor.forward();								
+			rightMotor.forward();
 			}	
+			}
 		}
 		
+		
+	public int calcP(int error){	
+		int correction;
+	
+		if (error<0){
+			error = error* (-1);
+		}
+		correction = (int)(PConstant*(double)error);
+	
+		if (correction>= motorStraight){
+			correction = MaxCorrection;
+		}
+		return correction;
 	}
-
 	
 	@Override
 	public int readUSDistance() {
